2cb672359023cb3a87ceb1e6ceb41cee31b7d7f4
Kostya <niilsa121@gmail.com>
2021-11-24 17:55:37 +0300

    4 methods  and 8 tests add

--- src/main/java/com/epam/calculator/Calculator.java
+++ src/main/java/com/epam/calculator/Calculator.java
@@ -1,4 +1,19 @@
 package com.epam.calculator;
 
 public class Calculator {
+    public int add(int firstArgument, int secondArgument) {
+        return firstArgument + secondArgument;
+    }
+
+    public int difference(int firstArgument, int secondArgument) {
+        return firstArgument - secondArgument;
+    }
+
+    public int multiply(int firstArgument, int secondArgument) {
+        return firstArgument * secondArgument;
+    }
+
+    public int divide(int firstArgument, int secondArgument) {
+        return firstArgument / secondArgument;
+    }
 }
